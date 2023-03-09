1ed0e9554209ef9c8ca494940bc9dba6c23cd02f
Unit06 <machine29a@gmail.com>
2023-03-09 17:48:44 +0900

    start day 4

---
+++ day 4/day4_notes.py
@@ -0,0 +1,16 @@
+import random
+#import pi_module
+
+random_interger = random.randint(0, 10)
+print(random_interger)
+
+random_float = random.random()
+print(random_float)
+
+#make float random 0 ~ 5
+random_float2 = random.random() * 5
+print(random_float2)
+
+
+
+#print(pi_module.pi)
---
+++ day 4/heads_tails.py
@@ -0,0 +1,8 @@
+import random
+
+coin = random.randint(0, 1)
+
+if coin:
+    print("Tails")
+else:
+    print("Heads")
